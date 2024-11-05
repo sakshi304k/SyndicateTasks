b8baa0d5038f06a67fea2af076ea9797c3acb9e9
sakshi <sakshikishor_kothurkar@epam.com>
2024-11-05 11:56:53 +0530

    task02 second commit imported neccessary files for @LambdaURLConfig

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -1,11 +1,29 @@
 package com.task02;
 
 import com.amazonaws.services.lambda.runtime.Context;
+
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+
 import com.syndicate.deployment.model.RetentionSetting;
 
+import com.syndicate.deployment.model.lambda.url.AuthType;
+
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+
 import java.util.HashMap;
+
 import java.util.Map;
 
 @LambdaHandler(
